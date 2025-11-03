2bf08ab620093046d79b364715c513c5e218918d
Jackson Moore <jacksonsamuelmoore@gmail.com>
2025-11-03 16:21:29 +1100

    fix: add missing.

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,33 @@
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import { resolve } from 'node:path'
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { resolve } from "node:path";
+import { nitro } from "nitro/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ autoCodeSplitting: true }),
-    viteReact(),
-    tailwindcss(),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  },
-})
+	plugins: [
+		tanstackStart(),
+		nitro(
+			/*
+      // nitro config goes here, e.g.
+      { config: { preset: 'node-server' } }
+    */
+		),
+		viteReact(),
+		tailwindcss(),
+	],
+	test: {
+		globals: true,
+		environment: "jsdom",
+	},
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		port: 3000,
+	},
+});
